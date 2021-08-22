c0660ec41565f5882747b2241b87b49ea73501dc
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2021-08-22 12:39:47 +0200

    Version bump (#54)
    * upgrade

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ If you don't have YunoHost, please consult [the guide](https://yunohost.org/#/in
 
 Alternative front-end to YouTube
 
-**Shipped version:** 28.07.21~ynh2
+**Shipped version:** 21.08.22~ynh1
 
 **Demo:** https://invidious.site/
 
--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,7 +4,7 @@
 # COMMON VARIABLES
 #=================================================
 
-version_commit=5c76cdaad9da2330311e78fc4e451c40e88a9598
+version_commit=0db23f925225f50ed574c69abbb0bd5de58bf709
 
 # dependencies used by the app
 pkg_dependencies="apt-transport-https libssl-dev libxml2-dev libyaml-dev libgmp-dev libreadline-dev postgresql librsvg2-bin libsqlite3-dev zlib1g-dev libevent-dev pkg-config libpcre3-dev"
