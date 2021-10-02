cd0de90e5c53aeb77e3b2c9b8be1d608ba0b8901
ericgaspar <junk.eg@free.fr>
2021-10-02 16:23:12 +0200

    Reorder

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,7 +7,7 @@
 version_commit=0db23f925225f50ed574c69abbb0bd5de58bf709
 
 # dependencies used by the app
-pkg_dependencies="apt-transport-https libssl-dev libxml2-dev libyaml-dev libgmp-dev libreadline-dev postgresql librsvg2-bin libsqlite3-dev zlib1g-dev libevent-dev pkg-config libpcre3-dev"
+pkg_dependencies="apt-transport-https libssl-dev libxml2-dev libyaml-dev libgmp-dev libreadline-dev postgresql librsvg2-bin imagemagick libsqlite3-dev zlib1g-dev libevent-dev pkg-config libpcre3-dev"
 
 #=================================================
 # PERSONAL HELPERS
