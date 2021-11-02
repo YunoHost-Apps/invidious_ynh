03494678f166a03637ef31ddbdfd981ee00bb9bc
ericgaspar <junk.eg@free.fr>
2021-11-02 15:51:51 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,7 +4,7 @@
 # COMMON VARIABLES
 #=================================================
 
-version_commit=7b2aa5f98e140ae989eb4f61cd8a94e3127de025
+version_commit=afb284fa9baa4c199f6770651f5a5d4ef01f2d9f
 
 # dependencies used by the app
 pkg_dependencies="apt-transport-https libssl-dev libxml2-dev libyaml-dev libgmp-dev libreadline-dev postgresql librsvg2-bin imagemagick libsqlite3-dev zlib1g-dev libevent-dev pkg-config libpcre3-dev"
