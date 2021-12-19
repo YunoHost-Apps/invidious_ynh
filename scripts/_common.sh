1331610b8fd3388b69ff2ac191e830537b16843d
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2021-12-19 23:39:00 +0100

    Testing (#68)
    * 4.3

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,7 +4,7 @@
 # COMMON VARIABLES
 #=================================================
 
-version_commit=afb284fa9baa4c199f6770651f5a5d4ef01f2d9f
+version_commit=7f3ef1229722adba65ec60caa3d209d18b47fd2e
 
 # dependencies used by the app
 pkg_dependencies="apt-transport-https libssl-dev libxml2-dev libyaml-dev libgmp-dev libreadline-dev postgresql librsvg2-bin imagemagick libsqlite3-dev zlib1g-dev libevent-dev pkg-config libpcre3-dev"
