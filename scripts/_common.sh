67012cbe60fa9ce3b8845718cd11b9e302f7489e
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2022-01-20 15:40:49 +0100

    Testing (#73)
    * Add config panel

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,7 +4,7 @@
 # COMMON VARIABLES
 #=================================================
 
-version_commit=b65dced646acf56f914a8916fd411237a141ecce
+version_commit=8b0ed8b110752327b98565e4ecc4faf437c0bb3b
 
 # dependencies used by the app
 pkg_dependencies="apt-transport-https libssl-dev libxml2-dev libyaml-dev libgmp-dev libreadline-dev postgresql librsvg2-bin imagemagick libsqlite3-dev zlib1g-dev libevent-dev pkg-config libpcre3-dev"
