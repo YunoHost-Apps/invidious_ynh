ac0444699b4a7e911e5ee3dd8f8dff552e9bc95c
ericgaspar <junk.eg@free.fr>
2022-08-12 09:37:27 +0200

    Upgrade to upstream

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,7 +4,7 @@
 # COMMON VARIABLES
 #=================================================
 
-version_commit=4ab54f284c8585f2edec44a4b0d369813389e142
+version_commit=9cc041876944dde46cd8cc3e269995244c8e7724
 
 # dependencies used by the app
 pkg_dependencies="apt-transport-https libssl-dev libxml2-dev libyaml-dev libgmp-dev libreadline-dev postgresql librsvg2-bin imagemagick libsqlite3-dev zlib1g-dev libevent-dev pkg-config libpcre3-dev"
