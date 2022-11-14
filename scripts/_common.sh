0f98bbe87fe28c83af245d26efefcba0a1f3b379
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2022-11-14 17:51:16 +0100

    Upgrade

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,7 +4,7 @@
 # COMMON VARIABLES
 #=================================================
 
-version_commit=84cd4d6a5b1854bcf1058351e390cc0f4855c9d7
+version_commit=09942dee6621e7047a63dffcc61b3bbf78cef2c6
 # dependencies used by the app
 pkg_dependencies="apt-transport-https libssl-dev libxml2-dev libyaml-dev libgmp-dev libreadline-dev postgresql librsvg2-bin imagemagick libsqlite3-dev zlib1g-dev libevent-dev pkg-config libpcre3-dev"
 
