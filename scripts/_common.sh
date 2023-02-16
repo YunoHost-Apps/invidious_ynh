39eb95daf4a66ba491fde09bda29d01dd9d18ec2
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-02-16 19:23:58 +0100

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,7 +6,7 @@
 
 version_commit=7993784701686c057b85c57704537135e6326de9
 # dependencies used by the app
-pkg_dependencies="apt-transport-https libssl-dev libxml2-dev libyaml-dev libgmp-dev libreadline-dev postgresql librsvg2-bin imagemagick libsqlite3-dev zlib1g-dev libevent-dev pkg-config libpcre3-dev"
+#REMOVEME? pkg_dependencies="apt-transport-https libssl-dev libxml2-dev libyaml-dev libgmp-dev libreadline-dev postgresql librsvg2-bin imagemagick libsqlite3-dev zlib1g-dev libevent-dev pkg-config libpcre3-dev"
 
 #=================================================
 # PERSONAL HELPERS
