e89d34da3300ca6766e6f7d2f70518c42346094f
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-12-29 10:51:22 +0100

    update to upstream

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,7 +4,7 @@
 # COMMON VARIABLES
 #=================================================
 
-version_commit=c5b87e3b5e5cc7f7f5c8baa7732bd6d81d8f910a
+version_commit=97c4165f55c4574efb554c9dae8d919d08da1cdd
 
 #=================================================
 # PERSONAL HELPERS
